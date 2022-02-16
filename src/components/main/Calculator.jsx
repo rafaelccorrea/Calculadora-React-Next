1870c55395fb6ae76e743fd4c8ca48c4e4d23874
rafaelccorrea <rafaelgustavocorrea@gmail.com>
2022-02-15 22:43:42 -0300

    Refatorando componente de calculo

--- src/components/main/Calculator.jsx
+++ src/components/main/Calculator.jsx
@@ -3,72 +3,82 @@ import styles from './Calculator.module.css'
 import Button from '../buttons/Button'
 import Display from '../display/Display'
 
-const initialState = {
-    displayValue: '0',
-    clearDisplay: false,
-    operation: null,
-    values: [0, 0],
-    current: 0
-}
-
 export default function Calculator() {
-
-    const [state, setState] = useState({...initialState})
-
-    function clearMemory(){
-        setState({...initialState})
-    };
-
-    function setOperation (operation) {
-        if( state.current === 0){
-            setState({operation, current: 1, clearDisplay:true})
-        }else {
-            const equals = operation === "="
-            const currentOperation = state.operation
-            const values = [state.operation]
-            
-            try{
-                values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`)
-            }catch(e){
-                values[0] = state.values[0]
-            }
-            
-            values[1] = 0
-
-            setState({
-                displayValue: values[0],
-                operation: equals ? null : operation,
-                current: equals ? 0 : 1,
-                clearDisplay: !equals,
-                values
-            })
-        }
-    };
-
-    function addDigit (n) {
-         if (n === '.' && state.displayValue.includes('.')){
-             return
-         }
-
-         const clearDisplay = state.displayValue === '0' || state.clearDisplay,
-               currentValue = clearDisplay ? '' : state.displayValue,
-               displayValue = currentValue + n,
-               values = [...state.values]
-                console.log(values)
-               setState({ ...state, displayValue, clearDisplay: false })
-
-         if(n !== '.'){
-            const newValue = parseFloat(displayValue),
-                values = [state.values]
- 
-            values[state.current] = newValue
-            setState({ ...state, displayValue, clearDisplay: false, values })
-        }
-    };
+    const [displayValue, setDisplayValue] = useState("0");
+    const [clearDisplay, setClearDisplay] = useState(false);
+    const [operationState, setOperationState] = useState(null);
+    const [values, setValues] = useState([0, 0]);
+    const [current, setCurrent] = useState(0);
+   
+    function clearMemory() {
+      setDisplayValue("0");
+      setClearDisplay(false);
+      setOperationState("");
+      setValues([0, 0]);
+      setCurrent(0);
+    }
+   
+    function setOperation(operation) {
+      if (current === 0) {
+        setOperationState(operation);
+        setCurrent(1);
+        setClearDisplay(true);
+      } else if (current === 1) {
+        const equals = operation === "=";
+        const currentOperation = operationState;
+   
+        const clonedValues = [...values];
+        const resultOfOperation = clonedValues.reduce((accumulator, current) => {
+          if (currentOperation === "+") {
+            accumulator += current;
+            clonedValues[0] = accumulator;
+          } else if (currentOperation === "-") {
+            accumulator -= current;
+            clonedValues[0] = accumulator;
+          } else if (currentOperation === "/") {
+            accumulator /= current;
+            clonedValues[0] = accumulator;
+          } else if (currentOperation === "*") {
+            accumulator *= current;
+            clonedValues[0] = accumulator;
+          }
+   
+          return accumulator;
+        });
+   
+        values[0] = resultOfOperation;
+        values[1] = 0;
+   
+        setDisplayValue(String(values[0]));
+        setOperationState(equals ? null : operation);
+        setCurrent(equals ? 0 : 1);
+        setClearDisplay(!equals);
+        setValues(values);
+      }
+    }
+   
+    function addDigit(n) {
+      if (n === "." && displayValue.includes(".")) {
+        return;
+      }
+   
+      const isDisplayCleaned = displayValue === "0" || clearDisplay;
+      const currentValue = isDisplayCleaned ? "" : displayValue;
+      const valueOfDisplay = currentValue + n;
+      setDisplayValue(valueOfDisplay);
+      setClearDisplay(false);
+   
+      if (n !== ".") {
+        const newValue = parseFloat(valueOfDisplay);
+        const arrayOfValues = [...values];
+        arrayOfValues[current] = newValue;
+        setValues(arrayOfValues);
+      }
+    }
 
     return (
         <div className={styles.calculator}>
-            <Display value={state.displayValue}/>
+            <Display value={displayValue}/>
             <Button label="AC" click={clearMemory} triple/>
             <Button label="/" click={setOperation} operation/>
             <Button label="7" click={addDigit}/>
